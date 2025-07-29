896a912981b64930aa455190da79a7955d2c9419
lnqbat <b.lonqueu98@gmail.com>
2025-07-29 15:45:10 +0200

    Add async_generator.py: Implement async_generator coroutine to yield random floats

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Async Generator"""
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """Write a coroutine called async_generator that takes no arguments"""
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
