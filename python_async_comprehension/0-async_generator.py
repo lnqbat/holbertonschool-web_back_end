2c902e07d760a94d95b407e7fc4844f12ca413c0
lnqbat <b.lonqueu98@gmail.com>
2025-07-29 16:29:06 +0200

    Refactor async_generator loop variable for clarity

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -7,6 +7,6 @@ from typing import AsyncGenerator
 
 async def async_generator() -> AsyncGenerator[float, None]:
     """Write a coroutine called async_generator that takes no arguments"""
-    for i in range(10):
+    for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
