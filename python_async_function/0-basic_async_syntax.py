cbfe8c183214aaad751b3b544838702aa33c897c
lnqbat <b.lonqueu98@gmail.com>
2025-07-29 13:36:28 +0200

    Add 0-basic_async_syntax.py: Implement wait_random function to demonstrate basic async functionality

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""The basics of async"""
+import asyncio
+import random
+
+async def wait_random(max_delay: int = 10) -> float:
+    """takes in an integer argument"""
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
