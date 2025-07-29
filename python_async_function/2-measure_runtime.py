a43509adb710f62aa48f44e745998b484c9f1f58
lnqbat <b.lonqueu98@gmail.com>
2025-07-29 14:54:41 +0200

    Fix formatting: Add a blank line for improved readability in 2-measure_runtime.py

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -6,6 +6,7 @@ from typing import Callable
 
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
+
 def measure_time(n: int, max_delay: int) -> float:
     """A measure_time function with integers"""
     start = time.perf_counter()
