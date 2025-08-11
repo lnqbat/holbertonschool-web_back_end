41c9c26692347b9cd2247054ac7d1ab3e480196c
lnqbat <b.lonqueu98@gmail.com>
2025-08-11 14:42:11 +0200

    add doc code

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""Implement a method named get_page"""
 import csv
 from typing import List, Tuple
 
@@ -18,6 +19,7 @@ class Server:
         self.__dataset = None
 
     def dataset(self) -> List[List]:
+        """Cached dataset"""
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -26,6 +28,7 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """get_page with default value 1 and page_size with default value 10"""
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
 
