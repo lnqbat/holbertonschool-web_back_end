5ff3eea13bec2e5e5ee13f4b5a472a333258e322
lnqbat <b.lonqueu98@gmail.com>
2025-08-12 11:16:00 +0200

    feat: add function to update topics in a school document by name

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""function that changes all topics of a school document based on the name"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """changes all topics"""
+    return mongo_collection.update_many(
+        { "name": name },
+        { "$set": { "topics": topics } }
+    )
+ 
