7beba5ebf1c9c91e705b5780166fa35c80021777
lnqbat <b.lonqueu98@gmail.com>
2025-08-13 10:57:50 +0200

    fix: standardize string quotes in MongoDB client connection

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -5,7 +5,7 @@ from pymongo import MongoClient
 
 
 if __name__ == "__main__":
-    client = MongoClient("mongodb://127.0.0.1:27017")
+    client = MongoClient('mongodb://127.0.0.1:27017')
     collection = client.logs.nginx
 
     total = collection.count_documents({})
