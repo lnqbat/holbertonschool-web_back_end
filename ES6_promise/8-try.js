68bae96859b0c88dba11cb8442e0e67efc0da4bf
lnqbat <b.lonqueu98@gmail.com>
2025-08-26 10:10:54 +0200

    fix: correct division order in divideFunction

--- ES6_promise/8-try.js
+++ ES6_promise/8-try.js
@@ -2,5 +2,5 @@ export default function divideFunction(numerator, denominator) {
   if (denominator === 0) {
     throw new Error('cannot divide by 0');
   }
-  return denominator / numerator;
+  return numerator / denominator;
 }
