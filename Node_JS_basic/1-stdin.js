59ac73a4541cb952e40942f6be271bbb9d568a0f
lnqbat <b.lonqueu98@gmail.com>
2025-09-01 11:46:38 +0200

    fix: replace console.log with process.stdout.write for consistent output

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,11 +1,9 @@
-/* eslint jest/require-hook: "off" */
-
 console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', (name) => {
-  console.log(`Your name is: ${name.toString().trim()}`);
+  process.stdout.write(`Your name is: ${name.toString().trim()}\n`);
 });
 
 process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
