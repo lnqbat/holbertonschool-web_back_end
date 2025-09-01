7e02a1752786d1f953e7adbcd6ef6bffa7e3767e
lnqbat <b.lonqueu98@gmail.com>
2025-09-01 12:52:04 +0200

    fix: replace console.log with process.stdout.write for welcome message

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,14 +1,13 @@
-#!/usr/bin/node
+/*eslint-disable*/
+console.log("Welcome to Holberton School, what is your name?");
 
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.setEncoding("utf8");
 
-process.stdin.setEncoding('utf8');
-
-process.stdin.on('data', (name) => {
-  const name = name.toString().trim();
+process.stdin.on("data", (buff) => {
+  const name = buff.toString().trim();
   process.stdout.write(`Your name is: ${name}\n`);
 });
 
-process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
+process.stdin.on("end", () => {
+  process.stdout.write("This important software is now closing\n");
 });
