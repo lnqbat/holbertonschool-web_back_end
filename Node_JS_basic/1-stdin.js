702b6f6023263fa837d423bd23f7bc139291c8a9
lnqbat <b.lonqueu98@gmail.com>
2025-09-01 15:18:32 +0200

    fix: standardize string quotes for consistency in 1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,13 +1,12 @@
-/*eslint-disable*/
-console.log("Welcome to Holberton School, what is your name?");
+console.log('Welcome to Holberton School, what is your name?');
 
-process.stdin.setEncoding("utf8");
+process.stdin.setEncoding('utf8');
 
-process.stdin.on("data", (buff) => {
+process.stdin.on('data', (buff) => {
   const name = buff.toString().trim();
   process.stdout.write(`Your name is: ${name}\n`);
 });
 
-process.stdin.on("end", () => {
-  process.stdout.write("This important software is now closing\n");
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
