03b53ba2ac6dd5e835a70703641edd3dba36f341
lnqbat <b.lonqueu98@gmail.com>
2025-09-01 14:24:17 +0200

    fix: format code for consistency and improve readability

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,7 +1,7 @@
-const http = require('http')
+const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200,{'Content-Type': 'text/plain'});
-    res.end('Hello Holberton School!');
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello Holberton School!');
 });
 server.listen(1245);
