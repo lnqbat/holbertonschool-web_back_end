ecc493c565af97cbbedfb982cf32772c3f9b140c
lnqbat <b.lonqueu98@gmail.com>
2025-09-02 10:09:46 +0200

    fix: use const for input variable in stdin.js for clarity

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,8 +3,8 @@ console.log('Welcome to Holberton School, what is your name?');
 process.stdin.setEncoding('utf-8');
 
 process.stdin.on('data', (name) => {
-  name = name.toString();
-  process.stdout.write(`Your name is: ${name}`);
+  const input = name.toString();
+  process.stdout.write(`Your name is: ${input}`);
 });
 
 process.stdin.on('end', () => {
