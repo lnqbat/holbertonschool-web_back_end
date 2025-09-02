3242f769df13068774f3c42718c4e0e4605361e3
lnqbat <b.lonqueu98@gmail.com>
2025-09-02 10:08:04 +0200

    fix: add missing semicolons for consistency in 6-http_express.js

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -1,6 +1,7 @@
-const express = require('express')
+const express = require('express');
+
 const app = express();
-const port = 1245
+const port = 1245;
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
